548184f6fff2f021ff790de4d36483d5e6d1c8a6
Yang Zhang <>
2018-06-26 11:21:23 -0700

    Update codes.

--- entertainment/novel/items.py
+++ entertainment/novel/items.py
@@ -9,17 +9,32 @@ import scrapy
 
 
 class NovelItem(scrapy.Item):
+    # 小说编号
+    nameId = scrapy.Field()
     # 小说名字
     name = scrapy.Field()
     # 作者
     author = scrapy.Field()
     # 小说地址
-    novelUrl = scrapy.Field()
+    novel_url = scrapy.Field()
     # 状态
-    serialStatus = scrapy.Field()
+    serial_status = scrapy.Field()
     # 连载字数
-    serialNumber = scrapy.Field()
+    serial_wordcount = scrapy.Field()
     # 文章类别
     category = scrapy.Field()
+
+
+
+class ContentItem(scrapy.Item):
     # 小说编号
-    nameId = scrapy.Field()
+    id_name = scrapy.Field()
+    # 章节名字
+    chaptername = scrapy.Field()
+    # 章节内容
+    chapter_content = scrapy.Field()
+    # 章节地址
+    chapter_url = scrapy.Field()
+    # 用于绑定章节顺序
+    sequence = scrapy.Field()
+
--- entertainment/novel/spiders/a23us.py
+++ entertainment/novel/spiders/a23us.py
@@ -1,10 +1,6 @@
 # -*- coding: utf-8 -*-
 import scrapy
-import logging
-from scrapy.spidermiddlewares.httperror import HttpError
-from twisted.internet.error import DNSLookupError, TCPTimedOutError
-
-from entertainment.novel.items import NovelItem
+from entertainment.novel.items import NovelItem, ContentItem
 
 
 class A23usSpider(scrapy.Spider):
@@ -16,50 +12,54 @@ class A23usSpider(scrapy.Spider):
     # 获得页面图书链接
     def parse(self, response):
         for book in response.xpath('//tr[@bgcolor="#FFFFFF"]'):
-            # print(book.extract())
-            novel_url = book.xpath('./td[1]/a[2]/@href').extract_first()
+            # 获取章节
+            novel_url = book.xpath('.//td[1]/a[2]/@href').extract()[0]
+
+            # 获取文章简介: 需要 xpath
+            # summary_url = response.c('.//td[1]/a[1]').extract()[0]
 
             item = NovelItem()
-            item['name'] = book.xpath('./td[1]/a[2]').extract_first()
-            item['novelUrl'] = novel_url
-            # yield item
+            # item['name'] = book.xpath('./td[1]/a[2]/text()').extract()[0]
+            # item['author'] = book.xpath('./td[3]/text()').extract()[0]
+            item['novel_url'] = novel_url
+            # item['serial_status'] = book.xpath('./td[6]/text()').extract()[0]
+            # item['serial_wordcount'] = book.xpath('./td[4]/text()').extract()[0]
+            # item['category'] = response.css('dl#content h2').re(u'(.+) - 文章列表')[0]
+            yield item
 
-            # self.logger.info("---------- get novel_url %s", novel_url)
-            # yield scrapy.http.Request(url=novel_url, callback=self.parse_book_chapter)
-        # return scrapy.Request(url='http://www.x23us.com/html/63/63265/', callback=self.parse_book_chapter)
-        yield scrapy.Request(url='http://www.x23us.com/html/63/63265/',
-                             callback=self.parse_book_chapter,
-                             errback=self.errback_httpbin)
-        # dont_filter=True)
+            # 获得全部章节
+            yield scrapy.http.Request(url=novel_url, callback=self.parse_book_chapter)
 
+        # TODO: 获取下一页
+
+    # 获取全部章节
     def parse_book_chapter(self, response):
-        # this would log http://www.example.com/some_page.html
-        self.logger.info('######Got successful response from {}'.format(response.url))
-        # allurls = response.xpath('//tr/td[@class="L"]')
-        # for url in allurls:
-        #     chaptername = url.xpath('.//a/text()').extract_first()
-        #     yield {
-        #         logging.log(chaptername)
-        #     }
+        self.logger.info('Got successful response from {}'.format(response.url))
+        num = 0
+        allurls = response.xpath('//tr')
+        for trurls in allurls:
+            tdurls = trurls.xpath('./td[@class="L"]')
+            for tdurl in tdurls:
+                num = num + 1
+                chapter_name = tdurl.xpath('.//a/text()').extract()[0]
+                chapter_url = response.url + tdurl.xpath('./a/@href').extract()[0]
+                self.logger.info('Got chapter url {}'.format(chapter_url))
+                # 验证章节是否已经存在
+                # rets = Sql.select_chapter(chapterurl)
+                # if rets[0] == 1:
+                #     print(u'章节已经存在了')
+                #     pass
+                # else:
+                yield scrapy.Request(chapter_url, callback=self.parse_content,
+                                     meta={'chapter_name': chapter_name, })
 
-    def errback_httpbin(self, failure):
-        # log all failures
-        self.logger.error(repr(failure))
+        # 获取文章内容
 
-        # in case you want to do something special for some errors,
-        # you may need the failure's type:
+    def parse_content(self, response):
+        chaptername = response.css('h1').extract()[0]
+        chapter_name_by_meta = response.meta['chapter_name']
 
-        if failure.check(HttpError):
-            # these exceptions come from HttpError spider middleware
-            # you can get the non-200 response
-            response = failure.value.response
-            self.logger.error('HttpError on %s', response.url)
-
-        elif failure.check(DNSLookupError):
-            # this is the original request
-            request = failure.request
-            self.logger.error('DNSLookupError on %s', request.url)
-
-        elif failure.check(TimeoutError, TCPTimedOutError):
-            request = failure.request
-            self.logger.error('TimeoutError on %s', request.url)
+        item = ContentItem()
+        contents = response.css('dd#contents').extract()
+        item['chapter_content'] = '\n   '.join(contents)
+        return item
