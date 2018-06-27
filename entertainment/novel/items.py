6134d695d7b043eeba8671499572b528684b7427
Yang Zhang <>
2018-06-27 15:19:11 -0700

    update settings.py and items.py

--- entertainment/novel/items.py
+++ entertainment/novel/items.py
@@ -8,35 +8,35 @@
 import scrapy
 
 
-class NovelItem(scrapy.Item):
+class BookItem(scrapy.Item):
     # 小说编号
-    novel_id = scrapy.Field()
+    book_id = scrapy.Field()
     # 小说名字
-    novel_name = scrapy.Field()
+    book_name = scrapy.Field()
     # 作者
-    author = scrapy.Field()
+    book_author = scrapy.Field()
     # 图片
-    picture = scrapy.Field()
+    book_image_url = scrapy.Field()
     # 内容简介
-    summary = scrapy.Field()
+    book_summary = scrapy.Field()
 
     # 小说地址
-    chapters_url = scrapy.Field()
+    book_chapters_url = scrapy.Field()
     # 状态
-    serial_status = scrapy.Field()
+    book_serial_status = scrapy.Field()
     # 连载字数
-    serial_wordcount = scrapy.Field()
+    book_serial_wordcount = scrapy.Field()
     # 文章类别
-    category = scrapy.Field()
+    book_category = scrapy.Field()
     # 最后更新
-    last_update = scrapy.Field()
+    book_last_update_time = scrapy.Field()
 
 
 class ChapterItem(scrapy.Item):
     # 小说编号（和上面的小说对应）
-    novel_id = scrapy.Field()
+    book_id = scrapy.Field()
     # 用于绑定章节顺序
-    sequence = scrapy.Field()
+    chapter_sequence = scrapy.Field()
     # 章节汉字
     chapter_seq_chinese = scrapy.Field()
     # 章节名字
@@ -47,7 +47,7 @@ class ChapterItem(scrapy.Item):
     # 章节地址
     chapter_url = scrapy.Field()
     # 是否已经更新
-    is_update = scrapy.Field()
+    chapter_is_update = scrapy.Field()
     # 最后更新日期
-    last_updated_time = scrapy.Field()
+    chapter_last_update_time = scrapy.Field()
 
--- entertainment/novel/settings.py
+++ entertainment/novel/settings.py
@@ -8,6 +8,7 @@
 #     https://doc.scrapy.org/en/latest/topics/settings.html
 #     https://doc.scrapy.org/en/latest/topics/downloader-middleware.html
 #     https://doc.scrapy.org/en/latest/topics/spider-middleware.html
+FEED_EXPORT_ENCODING = 'utf-8'
 
 BOT_NAME = 'novel'
 
@@ -16,10 +17,10 @@ NEWSPIDER_MODULE = 'novel.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'novel (+http://www.yourdomain.com)'
+USER_AGENT = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.162 Safari/537.36'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
@@ -27,7 +28,7 @@ ROBOTSTXT_OBEY = True
 # Configure a delay for requests for the same website (default: 0)
 # See https://doc.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 2
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
@@ -64,9 +65,9 @@ ROBOTSTXT_OBEY = True
 
 # Configure item pipelines
 # See https://doc.scrapy.org/en/latest/topics/item-pipeline.html
-ITEM_PIPELINES = {
-   'novel.pipelines.NovelPipeline': 300,
-}
+# ITEM_PIPELINES = {
+#    'novel.pipelines.NovelPipeline': 300,
+# }
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://doc.scrapy.org/en/latest/topics/autothrottle.html
